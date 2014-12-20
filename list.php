add6e910bc3d99d3f93cd1107a85aedc8a551be4
Sadek Jabr <sadek.jabr@gmail.com>
2014-12-20 04:01:10 +0200

    test9

--- list.php
+++ list.php
@@ -22,7 +22,17 @@ if ($result->num_rows > 0) {
     	if ($row[helped] == "YES"){
     		echo "<li><strike>" . $row["name"] . "</strike></li>";
     	}else{	
-        	echo "<li>" . $row["name"] . "</li>";
+        	echo "<li>" . $row["name"] . ' 
+
+<form method="post" name="tasktitleform" action="">
+    <div class="title">
+        <h1 id="message"></h1>
+    </div>
+    <input class="tasktitle" id="tasktitle" name="tasktitle" type="hidden" value=" . $row["id"] . " />
+    <input class="save" value="Save" type="button" />
+</form>
+
+            </li>';
     	}
     }
     ?>
@@ -34,3 +44,34 @@ if ($result->num_rows > 0) {
 
 $conn->close();
 ?>
+<script type="text/javascript">
+$(document).ready(function(){     
+
+        $('.save').click(function(e){ 
+
+            var current_time = 123;
+            var tasktitle = $("input#tasktitle").val();
+            var dataString = 'current_time='+ current_time + '&tasktitle=' + tasktitle;
+            alert(dataString)
+
+            $.ajax({
+                type: "POST",
+                url: "helped.php",
+                data: dataString,
+                    
+                    success: function() {
+                        alert("HELP IS ON THE WAY!")
+                        $('.title').html("");
+                        $('#message').html("✓ Logged!")
+                        .hide()
+                     
+                            .fadeIn(1500, function() {
+                            $('#message').append("");
+                            });
+                        }
+            });
+            return false;
+
+        });
+    });
+</script>
