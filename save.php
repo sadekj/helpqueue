46338e745091db976a999e101e086d7d6fab472d
Sadek Jabr <sadek.jabr@gmail.com>
2014-12-20 03:47:32 +0200

    test8

--- save.php
+++ save.php
@@ -15,14 +15,32 @@ $sql = "INSERT INTO student (id, name, helped)
 VALUES (0, '".$tasktitle."', 'NO')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+    echo "New record created successfully, Help is on the way";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
+  
+$sql = "SELECT * FROM student ORDER BY id ASC;";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    ?>
+    <ol>
+    <?php
+    while($row = $result->fetch_assoc()) {
+    	if ($row[helped] == "YES"){
+    		echo "<li><strike>" . $row["name"] . "</strike></li>";
+    	}else{	
+        	echo "<li>" . $row["name"] . "</li>";
+    	}
+    }
+    ?>
+	</ol>
+    <?php
+} else {
+    echo "0 results";
+}
 
 $conn->close();
-?>
-
-<?php
-
 ?>
